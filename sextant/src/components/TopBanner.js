f50372774fd5e7cbfb732ebc32e6d1cbe81ccd46
Shivproj <shivindrakanti@gmail.com>
2023-05-22 11:02:13 +0530

    card commit

---
+++ sextant/src/components/TopBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TopBanner =()=> {
+  return (
+    <div className="top- banner">
+        <h1>This is a top-banner</h1>
+      
+    </div>
+  )
+}
+
+export default TopBanner
