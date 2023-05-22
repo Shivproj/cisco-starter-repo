b267cfc253df037150afd5b685093aeb6960d64e
Shivproj <shivindrakanti@gmail.com>
2023-05-22 11:39:47 +0530

    task 3

--- sextant/src/components/CardComp.js
+++ sextant/src/components/CardComp.js
@@ -1,13 +1,29 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import {useEffect, useState} from 'react';
+
 
 function CardComp() {
+  const [ip,setIp ] = useState();
+  useEffect(() => {
+   
+    fetch('https://api.ipify.org?format=json')         
+    .then(response => response.json())
+    .then(data => setIp(data.ip));
+     
+  }, [])
+
   return (
+   
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
+    
+            <p>{ip}</p>
+          
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
+          
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
